4ce1446af6e186d0a3c466d2a1b02868c011cfd3
La svedese <5554930+sarasjodin@users.noreply.github.com>
2026-08-05 19:30:34 +0200

    update server.js with prod endpoint url

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Zupreme API running on container port ${port}`);
-  console.log(
-    'Local health endpoint (Docker): http://localhost:3001/api/health',
-  );
+  if (process.env.NODE_ENV === 'development') {
+    console.log('Local health endpoint: http://localhost:3001/api/health');
+  } else {
+    console.log(
+      'Health endpoint: https://zupreme-restaurant-api.sarasjodin.se/api/health',
+    );
+  }
 });
